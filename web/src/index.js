e0dcefaab52314fe39d7d8f8e33b679eb908083d
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-01-15 20:07:05 -0400

    [Video3] React basic concepts

---
+++ web/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom'; // Allow React connect to elements tree
+import App from './App'; //
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// JSX = JavaScript+Html
